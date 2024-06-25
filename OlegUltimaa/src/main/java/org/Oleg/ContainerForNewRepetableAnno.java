39bab8e5317a2342540a127e0a0b6ae42478db1c
Vlagogaik <100479b@mail.ru>
2024-06-25 09:23:06 +0300

    Finish IO

--- OlegUltimaa/src/main/java/org/Oleg/ContainerForNewRepetableAnno.java
+++
@@ -1,9 +0,0 @@
-package org.Oleg;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface ContainerForNewRepetableAnno {
-    NewRepetableAnno[] value();
-}
