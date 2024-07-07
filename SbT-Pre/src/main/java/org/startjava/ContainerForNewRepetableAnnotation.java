3fe845adb71e5bd5ea49740d98d452a646d7e3cc
Vlagogaik <125185138+Vlagogaik@users.noreply.github.com>
2024-07-07 12:47:09 +0300

    Delete SbT-Pre directory

--- SbT-Pre/src/main/java/org/startjava/ContainerForNewRepetableAnnotation.java
+++
@@ -1,9 +0,0 @@
-package org.startjava;
-
-import java.lang.annotation.*;
-
-@Retention(RetentionPolicy.RUNTIME)
-@Target(ElementType.METHOD)
-public @interface ContainerForNewRepetableAnnotation {
-    NewRepetableAnnotation[] value();
-}
