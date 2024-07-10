3511aa6280982ea1b42a6e425ab0ab8a6bf4f7ff
Vlagogaik <125185138+Vlagogaik@users.noreply.github.com>
2024-07-10 12:10:45 +0300

    Delete spring/restJPA directory

--- spring/restJPA/src/main/java/org/rest/repository/CartRepository.java
+++
@@ -1,8 +0,0 @@
-package org.rest.repository;
-
-import org.rest.data.Cart;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CartRepository extends JpaRepository<Cart, Long> {
-    long save();
-}
