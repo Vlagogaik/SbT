298dec68919ae99fbdd32835db8e2f46bfd5c799
Vlagogaik <125185138+Vlagogaik@users.noreply.github.com>
2024-07-16 14:49:41 +0300

    Delete spring/restJDBCTemplate/target directory

--- spring/restJDBCTemplate/target/classes/data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO carts (id, promocode) VALUES (1, 'PROMO1');
-INSERT INTO clients (id, name, username, password, email, cart_id) VALUES (1, 'Oleg', 'oleg', '123', 'mail', 1);
-INSERT INTO products (id, name, price, count) VALUES (1, 'Product1', 100.00, 10);
-INSERT INTO products (id, name, price, count) VALUES (2, 'Product2', 100.00, 10);
-INSERT INTO products (id, name, price, count) VALUES (3, 'Product3', 100.00, 10);
-INSERT INTO products_carts (id, id_product, id_cart, count) VALUES (1, 1, 1, 10);
-INSERT INTO products_carts (id, id_product, id_cart, count) VALUES (2, 2, 1, 10);
-INSERT INTO products_carts (id, id_product, id_cart, count) VALUES (3, 3, 1, 10);
